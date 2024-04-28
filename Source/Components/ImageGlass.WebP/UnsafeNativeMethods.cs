71a6001a661adfdfb25b12d9d2247b21bdce9e31
Phap Dieu Duong <ddphap@gmail.com>
2024-04-28 12:10:09 +0800

    clean up libwebp

--- Source/Components/ImageGlass.WebP/UnsafeNativeMethods.cs
+++ Source/Components/ImageGlass.WebP/UnsafeNativeMethods.cs
@@ -5,7 +5,7 @@ namespace ImageGlass.WebP;
 
 
 [SuppressUnmanagedCodeSecurityAttribute]
-internal sealed partial class UnsafeNativeMethods
+internal sealed partial class LibWebp
 {
     [LibraryImport("kernel32.dll", EntryPoint = "RtlCopyMemory", SetLastError = false)]
     internal static partial void CopyMemory(IntPtr dest, IntPtr src, uint count);
--- Source/Components/ImageGlass.WebP/libwebpdemux.cs
+++ Source/Components/ImageGlass.WebP/libwebpdemux.cs
@@ -1,4 +1,22 @@
-﻿using System.Runtime.CompilerServices;
+﻿/*
+ImageGlass Project - Image viewer for Windows
+Copyright (C) 2010 - 2024 DUONG DIEU PHAP
+Project homepage: https://imageglass.org
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security;
 
@@ -6,7 +24,7 @@ namespace ImageGlass.WebP;
 
 
 [SuppressUnmanagedCodeSecurityAttribute]
-internal sealed partial class UnsafeNativeMethods
+internal sealed partial class LibWebpDemux
 {
     // from WebPAnimDecoder API
     private static readonly int WEBP_DEMUX_ABI_VERSION = 0x0107;
