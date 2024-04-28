30af801629c82d95cf464b03a197cb4dfd233a67
Phap Dieu Duong <ddphap@gmail.com>
2024-04-28 10:43:19 +0800

    refactor WebPWrapper

---
+++ Source/Components/ImageGlass.WebP/UnsafeNativeMethods.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+using System.Security;
+
+namespace ImageGlass.WebP;
+
+
+[SuppressUnmanagedCodeSecurityAttribute]
+internal sealed partial class UnsafeNativeMethods
+{
+    [LibraryImport("kernel32.dll", EntryPoint = "RtlCopyMemory", SetLastError = false)]
+    internal static partial void CopyMemory(IntPtr dest, IntPtr src, uint count);
+
+}
